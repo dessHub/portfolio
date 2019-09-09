63777ce251ab56f48c3f7a2081f8b2a47d515a8b
desshub <korirdesmond@MacBook-Pro-3.local>
2019-09-10 01:20:43 +0300

    Design Experties Section

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/App.scss';
 import Hero from './Hero';
 import About from './About';
+import Experties from './Experties';
 
 function App() {
   return (
     <div className="App">
        <Hero />
        <About />
+       <Experties />
     </div>
   );
 }
