0a65c027c811621b2b484d7ced23b5614d19df47
Desmond <desshub95@gmail.com>
2019-09-11 12:46:31 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About () {
     <div className="about">
         <h3 className="about__title">About Me</h3>
         <div className="about__copy">
-            <p>I'm a self motivated full stack web developer with 3+ years experience. My interest lies in building products that make our users’ lives better by leveraging data from user’s interaction with the product. I find joy solving common problems with technology and always looking forward to the next challenge. It's important to me that I add value to the teams I work with. I strive not to be successful, but to be of value.
+            <p>I'm a self-motivated full-stack web developer with 3+ years of experience. I am passionate about building products that make our users’ lives better by leveraging data from user’s interaction with the product. I find joy solving common problems with technology and always looking forward to the next challenge. It's important to me that I add value to the teams I work with. I strive not to be successful, but to be of value.
 Currently, I work as a Front-End Software Engineer at Andela and Lola – leveraging on both teams collaboration. From this partnership, I continue to learn more efficacious software engineering practices and how to effectively manage product teams.
 When I'm not coding I enjoy comedy series or hiking with my friends.
 </p>
